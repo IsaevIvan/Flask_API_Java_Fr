b5cb7acbc56bdfe9861216b43b380783bfa8ca9a
IvanIsaev <79138903153@yandex.ru>
2025-07-22 15:07:27 +0700

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask
+from enum import unique
+
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # Файл БД
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), nullable = False)
+    email = db.Column(db.String(100), nullable = False, unique = True)
+
+    def __repr__(self):
+        return f'<User{self.name}>'
+
+    def to_dict(self):
+        return {'id': self.id, 'name': self.name, 'email': self.email}
 
 
 if __name__ == '__main__':
